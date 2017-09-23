1fa558d26d40d97a536d21ba7d65e212aba6dda6
bugobliterator <siddharthbharatpurohit@gmail.com>
2017-09-23 22:40:52 +0530

    rejig gen_dsdl to operate on directories

--- gen_canard.py
+++ gen_canard.py
@@ -1,9 +1,9 @@
 import sys
 import os
+from glob import glob
 
-px4_tools_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(px4_tools_dir + "/modules/genmsg/src")
-sys.path.append(px4_tools_dir + "/modules/gencpp/src")
+parser_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(parser_dir + "/src")
 
 import genmsg.template_tools
 
@@ -11,6 +11,23 @@ msg_template_map = { 'msg.h.template':'@NAME@.h' }
 srv_template_map = { 'srv.h.template':'@NAME@.h' }
 
 if __name__ == "__main__":
-    genmsg.template_tools.generate_from_command_line_options(sys.argv,
-                                                             msg_template_map,
-                                                             srv_template_map)
+    from optparse import OptionParser
+    parser = OptionParser("[options] <root_directory>")
+    parser.add_option("-o", dest='outdir',
+                      help="directory in which to place output files")
+    parser.add_option("-e", dest='emdir',
+                      help="directory containing template files",
+                      default=sys.path[0])
+
+    (options, argv) = parser.parse_args(sys.argv)
+
+    if( not options.outdir or not options.emdir):
+        parser.print_help()
+        exit(-1)
+    file_list = [y.replace(argv[1]+'/','') for x in os.walk(argv[1]) for y in glob(os.path.join(x[0], '*.uavcan'))]
+    includepath = [':'.join([x[0].replace(argv[1]+'/',''), x[0]]) for x in os.walk(argv[1])]
+    for file_name in file_list:
+        print('')
+        if len(file_name) > 1:
+            genmsg.template_tools.generate_from_file(argv[1], file_name, os.path.dirname(file_name), options.outdir, options.emdir, includepath, msg_template_map, srv_template_map)
+
